e3720a038664949138b64c04653fb910a46720c0
Dennis Højbjerg Rose <drose16@student.aau.dk>
2021-02-18 14:07:30 +0100

    The guide can train the category information.

--- lda.py
+++ lda.py
@@ -12,6 +12,7 @@ from pyro.infer import SVI, JitTraceEnum_ELBO, TraceEnum_ELBO
 from pyro.optim import ClippedAdam
 
 import numpy as np
+from tqdm import tqdm
 
 logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.INFO)
 
@@ -41,16 +42,16 @@ def model(doc_word_data=None, category_data=None, args=None, batch_size=None):
             doc_word_data = doc_word_data[:, ind]
 
         if category_data is not None:
-            category_data = category_data[:, ind]
+            category_data = category_data[ind]
 
-        doc_categories = pyro.sample("doc_categories", dist.Categorical(category_weights), obs=category_data)
+        category_data = pyro.sample("doc_categories", dist.Categorical(category_weights), obs=category_data)
         # doc_topics = pyro.sample("doc_topics", dist.Dirichlet(topic_weights))
         with pyro.plate("words", args.num_words_per_doc):
             # The word_topics variable is marginalized out during inference,
             # achieved by specifying infer={"enumerate": "parallel"} and using
             # TraceEnum_ELBO for inference. Thus we can ignore this variable in
             # the guide.
-            word_topics = pyro.sample("word_topics", dist.Categorical(category_topics[doc_categories]),
+            word_topics = pyro.sample("word_topics", dist.Categorical(category_topics[category_data]),
                                       infer={"enumerate": "parallel"})
             doc_word_data = pyro.sample("doc_words", dist.Categorical(topic_words[word_topics]),
                                         obs=doc_word_data)
@@ -64,7 +65,7 @@ def make_predictor(args):
     layer_sizes = ([args.num_words] +
                    [int(s) for s in args.layer_sizes.split('-')] +
                    [args.num_topics])
-    logging.info('Creating MLP with sizes {}'.format(layer_sizes))  # Default layers: 1000 -> 100 -> 100 -> 8
+    logging.info('Creating MLP with sizes {}'.format(layer_sizes))  # Default layers: 1024 -> 100 -> 100 -> 8
     layers = []
     for in_size, out_size in zip(layer_sizes, layer_sizes[1:]):
         layer = nn.Linear(in_size, out_size)
@@ -87,7 +88,7 @@ def parametrized_guide(predictor, doc_word_data, category_data, args, batch_size
             lambda: torch.ones(args.num_topics, args.num_words),
             constraint=constraints.greater_than(0.5))
     with pyro.plate("topics", args.num_topics):
-        pyro.sample("topic_weights", dist.Gamma(topic_weights_posterior, 1.))
+        pyro.sample("topic_weights", dist.Gamma(topic_weights_posterior, 1.)).cuda()
         pyro.sample("topic_words", dist.Dirichlet(topic_words_posterior))
 
     category_weights_posterior = pyro.param(
@@ -107,22 +108,32 @@ def parametrized_guide(predictor, doc_word_data, category_data, args, batch_size
         lambda: torch.ones(args.num_topics),
         constraint=constraints.positive)
     with pyro.plate("documents", args.num_docs, batch_size) as ind:
-        pyro.sample("doc_categories", dist.Gamma(doc_category_posterior, 1.))
+        pyro.sample("doc_categories", dist.Categorical(doc_category_posterior))
 
     # Use an amortized guide for local variables.
     # pyro.module("predictor", predictor)
     # with pyro.plate("documents", args.num_docs, batch_size) as ind:
     #     doc_word_data = doc_word_data[:, ind]
-    #     category_data = category_data[:, ind]
+    #     category_data = category_data[ind]
     #     # The neural network will operate on histograms rather than word
     #     # index vectors, so we'll convert the raw data to a histogram.
-    #     counts = (torch.zeros(args.num_words, ind.size(0))
-    #               .scatter_add(0, doc_word_data, torch.ones(doc_word_data.shape)))
-    #     doc_topics = predictor(counts.transpose(0, 1))
-    #     pyro.sample("doc_topics", dist.Delta(doc_topics, event_dim=1))
+    #     counts = (torch.zeros(ind.size(0))
+    #               .scatter_add(0, category_data, torch.ones(category_data.shape)))
+    #     doc_categories = predictor(counts.transpose(0, 1))
+    #     pyro.sample("doc_categories", dist.Delta(doc_categories, event_dim=1))
+
+        # original:
+        # data = doc_word_data[:, ind]
+        # # The neural network will operate on histograms rather than word
+        # # index vectors, so we'll convert the raw data to a histogram.
+        # counts = (torch.zeros(args.num_words, ind.size(0))
+        #           .scatter_add(0, data, torch.ones(data.shape)))
+        # doc_topics = predictor(counts.transpose(0, 1))
+        # pyro.sample("doc_topics", dist.Delta(doc_topics, event_dim=1))
 
 
 def main(args):
+    logging.info(f"CUDA enabled: {torch.cuda.is_available()}")
     logging.info('Generating data')
     pyro.set_rng_seed(0)
     pyro.clear_param_store()
@@ -141,12 +152,13 @@ def main(args):
     optim = ClippedAdam({'lr': args.learning_rate})
     svi = SVI(model, guide, optim, elbo)
     logging.info('Step\tLoss')
-    for step in range(args.num_steps):
+    for step in tqdm(range(args.num_steps)):
         loss = svi.step(doc_word_data=doc_word_data, category_data=category_data, args=args, batch_size=args.batch_size)
         if step % 10 == 0:
             logging.info('{: >5d}\t{}'.format(step, loss))
     loss = elbo.loss(model, guide, doc_word_data=doc_word_data, category_data=category_data, args=args)
     logging.info('final loss = {}'.format(loss))
+    print("debug string")
 
 
 if __name__ == '__main__':
@@ -157,10 +169,10 @@ if __name__ == '__main__':
     parser.add_argument("-w", "--num-words", default=1024, type=int)
     parser.add_argument("-d", "--num-docs", default=1000, type=int)
     parser.add_argument("-wd", "--num-words-per-doc", default=64, type=int)
-    parser.add_argument("-n", "--num-steps", default=50, type=int)
+    parser.add_argument("-n", "--num-steps", default=1000, type=int)
     parser.add_argument("-l", "--layer-sizes", default="100-100")
     parser.add_argument("-lr", "--learning-rate", default=0.01, type=float)
-    parser.add_argument("-b", "--batch-size", default=8, type=int)
+    parser.add_argument("-b", "--batch-size", default=32, type=int)
     parser.add_argument('--jit', action='store_true')
     args = parser.parse_args()
     main(args)
