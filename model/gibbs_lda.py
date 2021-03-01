94d27d08688b8d04157379818b224b2a0c9ea8ab
Simba <rech16@student.aau.dk>
2021-03-01 12:19:25 +0100

    revert to original since parra didnt work

--- model/gibbs_lda.py
+++ model/gibbs_lda.py
@@ -1,60 +1,36 @@
-import re
+import itertools
 import time
 from functools import partial
 from multiprocessing import Pool
-
+import scipy.sparse as sp
 import numpy as np
-from nltk.corpus import stopwords
 from tqdm import tqdm
+from sklearn.feature_extraction.text import CountVectorizer
+
 from preprocess.preprocessing import preprocessing
 
 
-# def preprocessing(path: str):
-#     documents, category = preprocessing(path)
-#     word2id = {}
-#     id2word = {}
-#     docs = []
-#     current_document = []
-#     current_word_id = 0
-#     for document in tqdm(list(documents.values())[:1000]):
-#         segList = document.split(' ')
-#         for word in segList:
-#             word = word.lower().strip()
-#             if len(word) > 1 and not re.search('[0-9]', word) and word not in stopwords.words('danish'):
-#                 if word in word2id:
-#                     current_document.append(word2id[word])
-#                 else:
-#                     current_document.append(current_word_id)
-#                     word2id[word] = current_word_id
-#                     id2word[current_word_id] = word
-#                     current_word_id += 1
-#         docs.append(current_document)
-#         current_document = []
-#     return docs, word2id, id2word
-
-
-def gibbs(documents, doc_topic, topic_word):
-    print("Random initialization")
-    with Pool(processes=8) as p:
-        max_ = len(documents)
-        with tqdm(total=max_) as pbar:
-            for score in p.imap(partial(sampling, doc_topic, topic_word), enumerate(documents)):
-                pbar.update()
-
-
-def sampling(doc_topic, topic_word, doc):
-    d_index = doc[0]
-    for w_index, word in enumerate(doc[1]):
-        topic = Z[d_index][w_index]
-        doc_topic[d_index, topic] -= 1
-        topic_word[topic, word] -= 1
-        nz[topic] -= 1
-        pz = np.divide(np.multiply(doc_topic[d_index, :], topic_word[:, word]), nz)
-        topic = np.random.multinomial(1, pz / pz.sum()).argmax()
-        Z[d_index][w_index] = topic
-        doc_topic[d_index, topic] += 1
-        topic_word[topic, word] += 1
-        nz[topic] += 1
+# def gibbs(documents, doc_topic, topic_word):
+#     with Pool(processes=8) as p:
+#         max_ = len(documents)
+#         with tqdm(total=max_) as pbar:
+#             for score in p.imap(partial(sampling, doc_topic, topic_word), enumerate(documents)):
+#                 pbar.update()
+#
+#
+# def sampling(doc_topic, topic_word, doc):
+#     d_index = doc[0]
+#     for w_index, word in enumerate(doc[1]):
+#         topic = Z[d_index][w_index]
+#         doc_topic[d_index, topic] -= 1
+#         topic_word[topic, word] -= 1
+#         nz[topic] -= 1
+#         pz = np.divide(np.multiply(doc_topic[d_index, :], topic_word[:, word]), nz)
+#         topic = np.random.multinomial(1, pz / pz.sum()).argmax()
+#         Z[d_index][w_index] = topic
+#         doc_topic[d_index, topic] += 1
+#         topic_word[topic, word] += 1
+#         nz[topic] += 1
 
 
 def random_initialize(documents, doc_topic, topic_word):
@@ -102,16 +78,17 @@ if __name__ == '__main__':
     iterationNum = 50
     Z = []
     K = 10
-    corpora, data = preprocessing("data/2017_data.json")
-    data = [list(filter(lambda a: a != -1, corpora.doc2idx(doc))) for doc in data]
-    N = len(data)
+    corpora, documents = preprocessing("data/2017_data.json")
+    N = len(documents)
     M = len(corpora.token2id)
+    data = [list(filter(lambda a: a != -1, corpora.doc2idx(doc))) for doc in documents]
+
     document_topic_dist = np.zeros([N, K]) + alpha
     topic_word_dist = np.zeros([K, M]) + beta
     nz = np.zeros([K]) + M * beta
     random_initialize(data, document_topic_dist, topic_word_dist)
     for i in tqdm(range(0, iterationNum)):
-        gibbs(data, document_topic_dist, topic_word_dist)
+        gibbsSampling(data, document_topic_dist, topic_word_dist)
         print(time.strftime('%X'), "Iteration: ", i, " Completed", " Perplexity: ", perplexity(data))
 
     topic_words = []
