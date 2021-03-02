0ea0ce1eb8f05f4e794ad617a1878e6798b9954a
Simba <rech16@student.aau.dk>
2021-03-02 09:18:44 +0100

    working gibbs sampling model

--- model/gibbs_lda.py
+++ model/gibbs_lda.py
@@ -1,37 +1,8 @@
-import itertools
 import pickle
 import time
-from functools import partial
-from multiprocessing import Pool
-import scipy.sparse as sp
+
 import numpy as np
 from tqdm import tqdm
-from sklearn.feature_extraction.text import CountVectorizer
-
-from preprocess.preprocessing import preprocessing
-
-
-# def gibbs(documents, doc_topic, topic_word):
-#     with Pool(processes=8) as p:
-#         max_ = len(documents)
-#         with tqdm(total=max_) as pbar:
-#             for score in p.imap(partial(sampling, doc_topic, topic_word), enumerate(documents)):
-#                 pbar.update()
-#
-#
-# def sampling(doc_topic, topic_word, doc):
-#     d_index = doc[0]
-#     for w_index, word in enumerate(doc[1]):
-#         topic = Z[d_index][w_index]
-#         doc_topic[d_index, topic] -= 1
-#         topic_word[topic, word] -= 1
-#         nz[topic] -= 1
-#         pz = np.divide(np.multiply(doc_topic[d_index, :], topic_word[:, word]), nz)
-#         topic = np.random.multinomial(1, pz / pz.sum()).argmax()
-#         Z[d_index][w_index] = topic
-#         doc_topic[d_index, topic] += 1
-#         topic_word[topic, word] += 1
-#         nz[topic] += 1
 
 
 def random_initialize(documents, doc_topic, topic_word):
@@ -82,26 +53,29 @@ if __name__ == '__main__':
     K = 10
     with open("../preprocess/generated_files/doc_word_matrix", 'rb') as file:
         doc_word_matrix = pickle.load(file)
+    with open("../preprocess/generated_files/corpora", 'rb') as file:
+        corpora = pickle.load(file)
     doc_word_matrix = np.array(doc_word_matrix.to_dense(), dtype=int)
     N = doc_word_matrix.shape[0]
     M = doc_word_matrix.shape[1]
     doc_word_matrix = [np.nonzero(x)[0] for x in doc_word_matrix]
 
-
     document_topic_dist = np.zeros([N, K]) + alpha
     topic_word_dist = np.zeros([K, M]) + beta
     nz = np.zeros([K]) + M * beta
+
     random_initialize(doc_word_matrix, document_topic_dist, topic_word_dist)
     for i in tqdm(range(0, iterationNum)):
         gibbsSampling(doc_word_matrix, document_topic_dist, topic_word_dist)
         print(time.strftime('%X'), "Iteration: ", i, " Completed", " Perplexity: ", perplexity(doc_word_matrix))
 
     topic_words = []
+    id2token = {v:k for k,v in corpora.token2id.items()}
     maxTopicWordsNum = 10
     for z in range(0, K):
         ids = topic_word_dist[z, :].argsort()
         topic_word = []
         for j in ids:
-            topic_word.insert(0, corpora.id2token[j])
+            topic_word.insert(0, id2token[j])
         topic_words.append(topic_word[0: min(10, len(topic_word))])
     print(topic_words)
