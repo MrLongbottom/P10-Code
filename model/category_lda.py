cdd94216e6c9561ba397b9ea45df31810a03225c
Dennis Højbjerg Rose <drose16@student.aau.dk>
2021-03-02 10:21:24 +0100

    Removed old code and comments. Added comments about what happens throught the code

--- model/category_lda.py
+++ model/category_lda.py
@@ -21,9 +21,7 @@ logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.IN
 
 
 # This is a fully generative model of a batch of documents.
-# data is a [num_words_per_doc, num_documents] shaped array of word ids
-# (specifically it is not a histogram). We assume in this simple example
-# that all documents have the same number of words.
+# Documents do not need to include the same number of words.
 def model(doc_word_data=None, category_data=None, args=None, batch_size=None):
     # Globals.
     with pyro.plate("topics", args.num_topics):
@@ -35,12 +33,11 @@ def model(doc_word_data=None, category_data=None, args=None, batch_size=None):
 
     with pyro.plate("categories", args.num_categories):
         category_weights = pyro.sample("category_weights", dist.Gamma(1. / args.num_categories, 1.))
+        # TODO category weights might not be necessary in our model
         category_topics = pyro.sample("category_topics", dist.Dirichlet(topic_weights))
 
-    category_list = []
-    document_list = []
-
-    # Changed here from vector(with) to iteration to support varying number of words (num_words_per_doc).
+    doc_category_list = []
+    doc_word_list = []
 
     # Locals.
     for index, doc in enumerate(pyro.plate("documents", args.num_docs)):
@@ -54,22 +51,23 @@ def model(doc_word_data=None, category_data=None, args=None, batch_size=None):
         else:
             cur_category_data = None
 
-        category_list.append(
+        doc_category_list.append(
             pyro.sample("doc_categories_{}".format(doc), dist.Categorical(category_weights), obs=cur_category_data))
 
         with pyro.plate("words_{}".format(doc), args.num_words_per_doc[doc]):
-            # The word_topics variable is marginalized out during inference,
-            # achieved by specifying infer={"enumerate": "parallel"} and using
-            # TraceEnum_ELBO for inference. Thus we can ignore this variable in
-            # the guide.
             word_topics = pyro.sample("word_topics_{}".format(doc),
-                                      dist.Categorical(category_topics[int(category_list[index].item())]))  # TODO Remove enum parallel?
+                                      dist.Categorical(category_topics[int(doc_category_list[index].item())]))
+            # TODO Enum parallel/sequential optimizing?
 
-            document_list.append(pyro.sample("doc_words_{}".format(doc), dist.Categorical(topic_words[word_topics]),
+            doc_word_list.append(pyro.sample("doc_words_{}".format(doc), dist.Categorical(topic_words[word_topics]),
                                              obs=cur_doc_word_data))
 
-    results = {"topic_weights": topic_weights, "topic_words": topic_words, "doc_word_data": document_list,
-               "category_weights": category_weights, "category_topics": category_topics, "category_data": category_list}
+    results = {"topic_weights": topic_weights,
+               "topic_words": topic_words,
+               "doc_word_data": doc_word_list,
+               "category_weights": category_weights,
+               "category_topics": category_topics,
+               "category_data": doc_category_list}
 
     return results
 
@@ -105,16 +103,19 @@ def parametrized_guide(doc_word_data, category_data, args, batch_size=None):
         lambda: torch.ones(args.num_categories),
         constraint=constraints.less_than(args.num_categories))
     for doc in pyro.plate("documents", args.num_docs, batch_size):
-        pyro.sample("doc_categories_{}".format(doc), dist.Delta(doc_category_posterior, event_dim=1), infer={'is_auxiliary': True})
+        pyro.sample("doc_categories_{}".format(doc), dist.Delta(doc_category_posterior, event_dim=1),
+                    infer={'is_auxiliary': True})  # TODO might be worth finding out why is_auxilliary is necessary here
+        # TODO word_topics param might be necessary now, without the enum = parallel in the model
 
 
 def main(args):
-    logging.info(f"CUDA enabled: {torch.cuda.is_available()}")
+    logging.info(f"CUDA available: {torch.cuda.is_available()}")
     logging.info('Generating data')
     pyro.set_rng_seed(0)
     pyro.clear_param_store()
     pyro.enable_validation(True)
 
+    # Debugging the trace of the model. For showing the shapes of the tensors through the model
     # tracemodel = functools.partial(model, args=args)
     # trace = poutine.trace(tracemodel).get_trace()
     # trace.compute_log_prob()  # optional, but allows printing of log_prob shapes
@@ -128,51 +129,56 @@ def main(args):
 
     # Loading data
     corpora, documents, category_corpora, category_list = preprocessing()
-    doc_word_data = [torch.tensor(list(filter(lambda a: a != -1, corpora.doc2idx(doc))), dtype=torch.int64) for doc in documents]
-    category_data = [torch.tensor(list(filter(lambda a: a != -1, category_corpora.doc2idx(cat))), dtype=torch.int64) for cat in category_list]
+    doc_word_data = [torch.tensor(list(filter(lambda a: a != -1, corpora.doc2idx(doc))), dtype=torch.int64)
+                     for doc in documents]
+    category_data = [torch.tensor(list(filter(lambda a: a != -1, category_corpora.doc2idx(cat))), dtype=torch.int64)
+                     for cat in category_list]
+
+    # Setting the new args
     args.num_words_per_doc = list(map(len, doc_word_data))
     args.num_words = len(corpora)
     args.num_docs = len(doc_word_data)
     args.num_categories = len(category_corpora)
+    args.num_topics = args.num_categories * 2
 
     # We'll train using SVI.
     logging.info('-' * 40)
     logging.info('Training on {} documents'.format(args.num_docs))
-    Elbo = JitTraceEnum_ELBO if args.jit else TraceEnum_ELBO
-    elbo = Elbo(max_plate_nesting=2)  # TODO problem may be in plate nesting or traceEnum. Look at forum post
-    optim = ClippedAdam({'lr': args.learning_rate})
+    Elbo = JitTraceEnum_ELBO if args.jit else TraceEnum_ELBO  # TODO test TraceEnum_ vs Trace_
+    elbo = Elbo(max_plate_nesting=2)  # TODO Changing the max plate nesting value might be worth looking at
+    optim = ClippedAdam({'lr': args.learning_rate})  # TODO try different learning rates
     svi = SVI(model, parametrized_guide, optim, elbo)
 
-    # loss_fn = pyro.infer.TraceEnum_ELBO().differentiable_loss
-    # loss = loss_fn(model, parametrized_guide, doc_word_data=doc_word_data, category_data=category_data, args=args, batch_size=args.batch_size)
-    # loss.backward()
-
-    # doc_word_data = torch.transpose(torch.stack(doc_word_data), 0, 1)  # TODO Make SVI run with lists of tensors
+    # If generating data from the model, turn category list to tensor
     # category_data = torch.Tensor(category_data)
 
     losses = []
 
+    # Training for num_steps iterations
     logging.info('Step\tLoss')
     for step in tqdm(range(args.num_steps)):
         loss = svi.step(doc_word_data=doc_word_data, category_data=category_data, args=args, batch_size=args.batch_size)
         losses.append(loss)
         if step % 10 == 0:
             logging.info('{: >5d}\t{}'.format(step, loss))
-    loss = elbo.loss(model, parametrized_guide, doc_word_data=doc_word_data, category_data=category_data, args=args)
+    loss = elbo.loss(model, parametrized_guide, doc_word_data=doc_word_data, category_data=category_data, args=args,
+                     batch_size=args.batch_size)
     logging.info('final loss = {}'.format(loss))
 
-    plt.plot(losses)
-    plt.title("ELBO")
-    plt.xlabel("step")
-    plt.ylabel("loss")
-    plt.savefig("loss-2017.png")
-    plt.show()
-
+    # Print params after training
     print('topic_weights_posterior = ', pyro.param("topic_weights_posterior"))
     print('topic_words_posterior = ', pyro.param("topic_words_posterior"))
     print('category_weights_posterior = ', pyro.param("category_weights_posterior"))
     print('category_topics_posterior = ', pyro.param("category_topics_posterior"))
     print('doc_category_posterior = ', pyro.param("doc_category_posterior"))
+
+    # Plot loss over iterations
+    plt.plot(losses)
+    plt.title("ELBO")
+    plt.xlabel("step")
+    plt.ylabel("loss")
+    plt.savefig("../loss-2017.png")
+    plt.show()
 
     # save model
     pyro.get_param_store().save("mymodelparams.pt")
@@ -186,14 +192,15 @@ def main(args):
 
 if __name__ == '__main__':
     assert pyro.__version__.startswith('1.5.2')
+
+    # Default args
     parser = argparse.ArgumentParser(description="Amortized Latent Dirichlet Allocation")
     parser.add_argument("-c", "--num-categories", default=32, type=int)
     parser.add_argument("-t", "--num-topics", default=64, type=int)
-    parser.add_argument("-w", "--num-words", default=1024,
-                        type=int)  # TODO Make num words be the number of unique words in dataset
+    parser.add_argument("-w", "--num-words", default=1024, type=int)
     parser.add_argument("-wd", "--num-words-per-doc", default=np.random.randint(low=5, high=1000, size=1000))
     parser.add_argument("-d", "--num-docs", default=1000, type=int)
-    parser.add_argument("-n", "--num-steps", default=1500, type=int)
+    parser.add_argument("-n", "--num-steps", default=1000, type=int)
     parser.add_argument("-l", "--layer-sizes", default="100-100")
     parser.add_argument("-lr", "--learning-rate", default=0.01, type=float)
     parser.add_argument("-b", "--batch-size", default=32, type=int)
