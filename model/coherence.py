0f361e5c880ac714f9783bb0bd1fbfe1e88fc473
MrLongbottom <perich16@student.aau.dk>
2021-03-05 14:15:58 +0100

    implemented topic difference

--- model/coherence.py
+++ model/coherence.py
@@ -1,7 +1,25 @@
+import numpy as np
 from gensim.models.coherencemodel import CoherenceModel
+from scipy.spatial import distance
 
 
 # supported measures = {'u_mass', 'c_v', 'c_uci', 'c_npmi'}
 def coherence(topics, doc2bow, dictionary, texts, coherence_measure = 'c_v'):
     cm = CoherenceModel(topics=topics, corpus=doc2bow, dictionary=dictionary, texts=texts, coherence=coherence_measure)
     return cm.get_coherence()
+
+
+def topic_diff(topic_word_dist):
+    topic_difference_matrix = np.zeros((topic_word_dist.shape[0]-1, topic_word_dist.shape[0]-1))
+    for topics in np.ndindex(topic_difference_matrix.shape):
+        # don't compare topics to themselves
+        if topics[0] == topics[1]:
+            continue
+        # fill out distance matrix with distance between topic1 and topic2
+        topic_difference_matrix[topics] = distance.jensenshannon(topic_word_dist[topics[0]], topic_word_dist[topics[1]])
+    return topic_difference_matrix
+
+
+def mean_topic_diff(topic_word_dist):
+    td = topic_diff(topic_word_dist)
+    return np.mean(td)
