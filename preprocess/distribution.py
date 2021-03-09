bc324c9c15c7fdadf8089a304c1e49dd6b8cf58c
MrLongbottom <perich16@student.aau.dk>
2021-03-09 13:51:00 +0100

    now also finds taxonomy distributions

--- preprocess/distribution.py
+++ preprocess/distribution.py
@@ -1,10 +1,18 @@
 import preprocess.preprocessing as pre
 
 if __name__ == '__main__':
-    doc2meta = pre.prepro_file_load('doc2author')
-    id2meta = pre.prepro_file_load('id2author')
+    doc2meta = pre.prepro_file_load('doc2category')
+    id2meta = pre.prepro_file_load('id2category')
     distribution = {}
     for k, v in doc2meta.items():
         distribution[v] = distribution.get(v, 0) + 1
     distribution = {id2meta[k]: v for k, v in distribution.items()}
+    taxonomy = False
+    if taxonomy:
+        distribution2 = {}
+        for k, v in distribution.items():
+            taxo = k.split('/')
+            for tax in taxo:
+                distribution2[tax] = distribution2.get(tax, 0) + v
+        distribution = distribution2
     print('test')
