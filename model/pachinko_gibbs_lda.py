7f82d7786c95dc5fe94d1bd853c6b61e95edf8a6
MrLongbottom <perich16@student.aau.dk>
2021-03-31 15:24:35 +0200

    merge

--- model/pachinko_gibbs_lda.py
+++ model/pachinko_gibbs_lda.py
@@ -211,8 +211,9 @@ if __name__ == '__main__':
 
     print("Starting Gibbs")
     for i in range(0, iterationNum):
-        gibbs_sampling(doc2word, word_topic_assignment, middle_counts, s2)
+        gibbs_sampling(doc2word, word_topic_assignment, middle_layers, topic_to_word)
         print(time.strftime('%X'), "Iteration: ", i, " Completed",
-              "Coherence: ", get_coherence(doc2bow, texts, corpora, s2_num, s2))
+              "Coherence: ", get_coherence(doc2bow, texts, corpora, layer_lengths[len(layer_lengths)-1], topic_to_word))
 
-    print(get_topics(corpora, s2_num, s2))
+
+    print(get_topics(corpora, s2_num, s2_to_words))
