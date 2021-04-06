3845731e8a45f78259a63550dd24e03bcfff1ea0
Simba <rech16@student.aau.dk>
2021-04-06 09:28:14 +0200

    made distribution a lot nice and able to print stuff

--- preprocess/distribution.py
+++ preprocess/distribution.py
@@ -1,31 +1,19 @@
 import numpy as np
 import preprocess.preprocessing as pre
 import matplotlib.pyplot as plt
+from collections import Counter
 
-# This file is meant to analyse and visualize various aspects of the dataset
-if __name__ == '__main__':
-    doc2meta = pre.prepro_file_load('doc2category', folder_name='full')
-    id2meta = pre.prepro_file_load('id2category', folder_name='full')
+
+def get_distribution(meta_data_name: str):
+    doc2meta = pre.prepro_file_load(f'doc2{meta_data_name}')
+    id2meta = pre.prepro_file_load(f'id2{meta_data_name}')
     distribution = {}
     for k, v in doc2meta.items():
         distribution[v] = distribution.get(v, 0) + 1
-    distribution = {id2meta[k]: v for k, v in distribution.items()}
-    #distribution = {k: v for k, v in distribution.items() if v > 140}
-    taxonomy = False
-    if taxonomy:
-        distribution2 = {}
-        for k, v in distribution.items():
-            taxo = k.split('/')
-            for tax in taxo:
-                distribution2[tax] = distribution2.get(tax, 0) + v
-        distribution = distribution2
-    sizes = list(distribution.values())
-    print("Mean: {}, Median: {}, Min: {}, Max: {}".format(np.mean(sizes), np.median(sizes), np.min(sizes), np.max(sizes)))
-    names = [str(x) for x in sizes]
-    fig1, ax1 = plt.subplots()
-    # Cumulative Histogram
-    #n, bins, patches = ax1.hist(sizes, 1000, density=True, histtype='step',
-    #                           cumulative=True, label='Empirical')
+    return {id2meta[k]: v for k, v in distribution.items()}
+
+
+def print_boxplot():
     # Boxplot
     box = ax1.boxplot(sizes)
     for line in box['medians']:
@@ -42,8 +30,44 @@ if __name__ == '__main__':
     for line in box['fliers']:
         x, y = line.get_xydata()[0]
         ax1.text(x - 0.06, y, '%.0f' % y, horizontalalignment='center', verticalalignment='center')
-    # Pie Chart
-    #ax1.pie(sizes, autopct='%1.1f', labels=names, startangle=90)
-    #ax1.axis('equal')
     plt.show()
 
+
+def print_histogram(meta_data):
+    # Cumulative Histogram
+    plt.hist(sizes, color='blue', edgecolor='black', bins=50)
+    plt.title(f'Histogram of Articles and {meta_data.capitalize() + "s"}')
+    plt.xlabel('Number of Articles')
+    plt.ylabel(f'Number of {meta_data.capitalize() + "s"}')
+    plt.savefig(f"{meta_data}_hist_plot.pdf")
+    plt.show()
+
+
+def print_pie_chart(meta_data):
+    # Pie Chart
+    plt.pie(sizes, autopct='%1.1f', labels=names, startangle=90)
+    plt.axis('equal')
+    plt.show()
+
+
+# This file is meant to analyse and visualize various aspects of the dataset
+if __name__ == '__main__':
+    meta_data = "author"
+    distribution = get_distribution(meta_data)
+    # distribution = {k: v for k, v in distribution.items() if v < 2000}
+
+    if meta_data == "taxonomy":
+        distribution2 = {}
+        for k, v in distribution.items():
+            taxo = k.split('/')
+            for tax in taxo:
+                distribution2[tax] = distribution2.get(tax, 0) + v
+        distribution = distribution2
+    sizes = list(Counter(distribution.values()))
+    print(f"Mean: {np.mean(sizes)}, "
+          f"Median: {np.median(sizes)}, "
+          f"Min: {np.min(sizes)}, "
+          f"Max: {np.max(sizes)}")
+    names = [str(x) for x in sizes]
+    fig1, ax1 = plt.subplots()
+    print_histogram(meta_data)
