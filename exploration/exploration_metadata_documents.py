cf0b1cd728152a6bff51abd255ad7e2350dd45a8
Dennis Højbjerg Rose <drose16@student.aau.dk>
2021-04-07 14:11:52 +0200

    Added comments, and prints author information as well

--- exploration/exploration_metadata_documents.py
+++ exploration/exploration_metadata_documents.py
@@ -21,16 +21,18 @@ if __name__ == '__main__':
     doc2pre = pre.prepro_file_load('doc2pre_text', folder_name='full')
     doc2raw = pre.prepro_file_load('doc2raw_text', folder_name='full')
     id2doc = pre.prepro_file_load('id2doc', folder_name='full')
+    doc2author = pre.prepro_file_load('doc2author', folder_name='full')
 
-    id2meta = pre.prepro_file_load('id2category', folder_name='full')
+    id2category = pre.prepro_file_load('id2category', folder_name='full')
     doc2meta = pre.prepro_file_load('doc2category', folder_name='full')
     value = "26. Frederik"
-    # id2meta = pre.prepro_file_load('id2author', folder_name='full')
+    id2author = pre.prepro_file_load('id2author', folder_name='full')
     # doc2meta = pre.prepro_file_load('doc2author', folder_name='full')
     # value = "Villy Dall"
 
-    metaID = find_id_from_value(id2meta, value)
+    metaID = find_id_from_value(id2category, value)
 
+    # get document IDs for documents with the given metadata
     documentIDs = []
     for key, val in doc2meta.items():
         if metaID == val:
@@ -38,16 +40,25 @@ if __name__ == '__main__':
 
     documents = {}
     documentsRaw = {}
+    docAuthors = {}
     docFileNames = {}
+    # get data based on document IDs
     for docID in documentIDs:
-        documents[docID] = doc2pre.get(docID)
-        documentsRaw[docID] = doc2raw.get(docID)
-        docFileNames[docID] = id2doc.get(docID)
+        documents[docID] = doc2pre[docID]
+        documentsRaw[docID] = doc2raw[docID]
+        docAuthors[docID] = doc2author[docID]
+        docFileNames[docID] = id2doc[docID]
 
+    # document list information
     print(f"{len(documents)} documents found\n")
+    print(f"{len(set(docAuthors.values()))} unique authors: ")
+    print(f"{[id2author[id] for id in set(docAuthors.values())]}\n")
+
+    # random examples of documents with ID, author, preprocessed data, and raw data
     print("10 random documents:")
     for count in range(10):
         id = random.choice(documentIDs)
-        print(id)
+        print(f"ID: {id}")
+        print(f"Author: {id2author[docAuthors[id]]}")
         print(documents[id])
         print(documentsRaw[id] + "\n")
