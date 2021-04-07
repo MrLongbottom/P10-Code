3e28c34fea6f99e8333273364adf09123f9564fe
Simba <rech16@student.aau.dk>
2021-04-07 11:37:10 +0200

    added full saving to all models

--- model/gibbs_lda.py
+++ model/gibbs_lda.py
@@ -8,37 +8,8 @@ from tqdm import tqdm
 
 from gibbs_utility import increase_count, decrease_count, perplexity, get_coherence, get_topics, \
     _conditional_distribution
+from model.save import Model
 from preprocess.preprocessing import prepro_file_load
-
-
-class Model:
-    def __init__(self, num_topics, alpha, eta, doc_topic, topic_word, name):
-        self.num_topics = num_topics
-        self.alpha = alpha
-        self.eta = eta
-        self.doc_topic = doc_topic
-        self.topic_word = topic_word
-        self.name = name
-
-    def to_dict(self):
-        return {"num_topic": self.num_topics,
-                "alpha": self.alpha,
-                "eta": self.eta,
-                "doc_topic": self.doc_topic,
-                "topic_word": self.topic_word}
-
-    def to_str(self):
-        return f"{self.num_topics}_{self.alpha}_{self.eta}_{self.name}"
-
-    def save_model(self, save_path=""):
-        full_path = save_path + self.to_str()
-        with open(full_path, "wb") as file_path:
-            pickle.dump(self.to_dict(), file_path)
-
-
-def load_model(load_path):
-    with open(load_path, 'rb') as file_path:
-        return pickle.load(file_path)
 
 
 def random_initialize(documents: List[np.ndarray]):
