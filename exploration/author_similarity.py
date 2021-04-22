ef46d0b3e2ac8ba52e28221787e255f5f85f6005
Dennis Højbjerg Rose <drose16@student.aau.dk>
2021-04-22 10:59:28 +0200

    Author-topic distribution is now normalized

--- exploration/author_similarity.py
+++ exploration/author_similarity.py
@@ -29,7 +29,12 @@ if __name__ == '__main__':
     model_path = "../model/models/90_0.01_0.1_author"
     model = load_model(model_path)
 
+    # Get author-topic distribution and normalize
     author_topic = model.doc_topic
+    for row in range(author_topic.shape[0]):
+        row_sum = sum(author_topic[row])
+        author_topic[row] = [val/row_sum for val in author_topic[row]]
+
     author_similarities = calculate_author_similarities(author_topic)
     sorted_author_similarities = dict(sorted(author_similarities.items(), key=lambda item: item[1]))
     top_author_pairs = list(sorted_author_similarities.items())[:10]
