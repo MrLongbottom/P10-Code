ba4aebd75f8f74ee376d869366f8e4e800cb96e3
Simba <rech16@student.aau.dk>
2021-04-23 12:06:01 +0200

    added a function to color specfic words within text

--- exploration/model_doc_topics.py
+++ exploration/model_doc_topics.py
@@ -1,21 +1,31 @@
 import random
+import re
+from typing import List
 
+import itertools
 import preprocess.preprocessing as pre
 from model.save import load_model
 from gibbs_utility import get_topics
 
 
+def color_words(words: List[str], text: str):
+    for word in words:
+        text = text.replace(" " + word + " ", " \\" + "colorbox{green}" + "{" + word + "} ")
+    return text
+
+
 if __name__ == '__main__':
-    corpora = pre.prepro_file_load("corpora", "full")
-    doc2pre = pre.prepro_file_load('doc2pre_text', folder_name='full')
-    doc2raw = pre.prepro_file_load('doc2raw_text', folder_name='full')
-    model = load_model("../model/models/90_0.01_0.1_standard")
+    corpora = pre.prepro_file_load("corpora")
+    doc2pre = pre.prepro_file_load('doc2pre_text')
+    doc2raw = pre.prepro_file_load('doc2raw_text')
+    model = load_model("../model/90_0.01_0.1_author")
     num_topics = model.num_topics
     topic_word_dist = model.topic_word
 
     docs = dict(enumerate(model.doc_topic))
     docs = [(k, v) for k, v in docs.items()]  # convert to list of (ID, topics)
-    sample_docs = random.sample(docs, 5)  # sample 5 random documents
+    # sample_docs = random.sample(docs, 5)  # sample 5 random documents
+    sample_docs = [docs[38668]]
     # convert topic probabilities to (ID, probability) pairs to know IDs after sorting
     sample_docs = [(doc[0], [(index, topic) for index, topic in enumerate(doc[1])]) for doc in sample_docs]
 
@@ -27,7 +37,7 @@ if __name__ == '__main__':
     for doc in sorted_doc_topic.items():
         doc_top_topics[doc[0]] = [doc[1][index] for index in range(3)]
 
-    topic_top_words = get_topics(corpora, num_topics, topic_word_dist)
+    topic_top_words = get_topics(corpora, num_topics, topic_word_dist, num_of_word_per_topic=50)
 
     # printing doc-topic -> topic-word connections
     print("Random documents with top topics:")
@@ -35,9 +45,10 @@ if __name__ == '__main__':
         id = doc[0]
         print(f"Document ID: {id}")
         print(doc2pre[id])
-        print(doc2raw[id] + "\n")
+        print(color_words(list(itertools.chain.from_iterable([topic_top_words[x] for x, y in doc[1]])),
+                          doc2raw[id]) + "\n")
 
         print("Top words in document top topics:")
-        for topic in doc[1]:
-            print(f"Topic ID/probability: {topic[0]}/{'{:.2f}'.format(topic[1])} {topic_top_words[topic[0]]}")
+        for topic_num, topic_percent in doc[1]:
+            print(f"Topic ID/probability: {topic_num}/{'{:.2f}'.format(topic_percent)} {topic_top_words[topic_num]}")
         print()
