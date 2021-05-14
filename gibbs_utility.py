f4324c29f4f8c9f083942eccc27c23d71012c8fb
Simba <rech16@student.aau.dk>
2021-05-14 12:56:55 +0200

    made so that we can compute multi

--- gibbs_utility.py
+++ gibbs_utility.py
@@ -1,10 +1,9 @@
 from typing import List
 
+import numpy as np
 from gensim.models import CoherenceModel
 from scipy.spatial import distance
 from tqdm import tqdm
-
-import numpy as np
 
 # supported measures = {'u_mass', 'c_v', 'c_uci', 'c_npmi'}
 from model.save import Model, load_model
@@ -32,7 +31,7 @@ def mean_topic_diff(topic_word_dist):
     return np.mean(td)
 
 
-def perplexity(documents: List[np.ndarray],  document_topic, document_topic_count, topic_word, topic_word_c) -> float:
+def perplexity(documents: List[np.ndarray], document_topic, document_topic_count, topic_word, topic_word_c) -> float:
     """
     Calculates the perplexity based on the documents given
     :param documents: a list of documents with word ids
@@ -47,7 +46,8 @@ def perplexity(documents: List[np.ndarray],  document_topic, document_topic_coun
     return np.exp(ll / (-n))
 
 
-def x_perplexity(documents: List[np.ndarray], feature_topic, feature_topic_c, topic_word, topic_word_c, doc2feature) -> float:
+def x_perplexity(documents: List[np.ndarray], feature_topic, feature_topic_c, topic_word, topic_word_c,
+                 doc2feature) -> float:
     """
     Calculates the perplexity based on the documents given
     :param documents: a list of documents with word ids
@@ -58,7 +58,32 @@ def x_perplexity(documents: List[np.ndarray], feature_topic, feature_topic_c, to
     for d, doc in documents:
         feature = doc2feature[d]
         for w in doc:
-            ll += np.log(((topic_word[:, w] / topic_word_c) * (feature_topic[feature, :] / feature_topic_c[feature])).sum())
+            ll += np.log(
+                ((topic_word[:, w] / topic_word_c) * (feature_topic[feature, :] / feature_topic_c[feature])).sum())
+            n += 1
+    return np.exp(ll / (-n))
+
+
+def triple_perplexity(documents: List[np.ndarray],
+                      author_topic, author_topic_c,
+                      category_topic, category_topic_c,
+                      topic_word, topic_word_c) -> float:
+    """
+    Calculates the perplexity based on the documents given
+    :param documents: a list of documents with word ids
+    :return: the perplexity of the documents given
+    """
+    n = 0
+    ll = 0.0
+    doc2author = prepro_file_load("doc2author")
+    doc2category = prepro_file_load("doc2category")
+    for d, doc in documents:
+        author = doc2author[d]
+        category = doc2category[d]
+        for w in doc:
+            ll += np.log(((topic_word[:, w] / topic_word_c) *
+                          (category_topic[category, :] / category_topic_c[category]) *
+                          (author_topic[author, :] / author_topic_c[author])).sum())
             n += 1
     return np.exp(ll / (-n))
 
@@ -108,7 +133,7 @@ def _conditional_distribution(doc_index, word, doc_topic, doc_topic_count, topic
     p_z /= np.sum(p_z)
     return p_z
 
-  
+
 def _conditional_distribution_combination(author, category, word, author_topic, author_topic_count,
                                           category_topic, category_topic_count, topic_word, topic_word_count):
     left = np.divide(topic_word[:, word], topic_word_count)
@@ -119,26 +144,42 @@ def _conditional_distribution_combination(author, category, word, author_topic, 
     p_z /= np.sum(p_z)
     return p_z
 
-  
-def compute_metrics_on_saved_model(save_model: str or Model, test_documents):
+
+def compute_metrics_on_saved_model(save_model: str or Model, test_documents, multi: bool = False):
     """
     Compute the three metrics (perplexity, coherence and topic diff) on a saved model
     :param save_model: the name of the model
     :param test_documents: the test documents we want to test on
     :return: a dict containing the results
     """
-    if type(save_model) == str:
+    if type(save_model) == str and not multi:
         loaded_model = load_model(save_model)
+    elif type(save_model) == str and multi:
+        loaded_model = load_model(save_model, True)
     else:
         loaded_model = save_model
 
     doc2bow, dictionary, texts = prepro_file_load('doc2bow'), prepro_file_load('corpora'), list(
         prepro_file_load('doc2pre_text').values())
-    model_perplexity = perplexity(test_documents, loaded_model.doc_topic, loaded_model.topic_word,
-                                  loaded_model.topic_word_count,
-                                  loaded_model.doc_topic_count)
-    model_coherence = get_coherence(doc2bow, dictionary, texts, save_model.num_topics, loaded_model.topic_word)
-    model_topic_difference = mean_topic_diff(loaded_model.topic_word)
+
+    if not multi:
+        model_perplexity = perplexity(test_documents,
+                                      loaded_model.doc_topic,
+                                      loaded_model.doc_topic_count,
+                                      loaded_model.topic_word,
+                                      loaded_model.topic_word_count)
+        model_coherence = get_coherence(doc2bow, dictionary, texts, loaded_model.num_topics, loaded_model.topic_word)
+        model_topic_difference = mean_topic_diff(loaded_model.topic_word)
+    else:
+        model_perplexity = triple_perplexity(test_documents,
+                                             loaded_model.feature_topic,
+                                             loaded_model.feature_topic_count,
+                                             loaded_model.feature2_topic,
+                                             loaded_model.feature2_topic_count,
+                                             loaded_model.topic_word,
+                                             loaded_model.topic_word_count)
+        model_coherence = get_coherence(doc2bow, dictionary, texts, loaded_model.num_topics, loaded_model.topic_word)
+        model_topic_difference = mean_topic_diff(loaded_model.topic_word)
     return {"perplexity": model_perplexity,
             "coherence": model_coherence,
             "topic_diff": model_topic_difference}
