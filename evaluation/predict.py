dcde044e2dcb4dcf43530e616624793f4304312f
PeterLangballeErichsen <p.erichsen@yahoo.dk>
2021-11-12 14:39:45 +0100

    prediction stuff

--- evaluation/predict.py
+++ evaluation/predict.py
@@ -16,12 +16,18 @@ _SQRT2 = np.sqrt(2)  # sqrt(2) with default precision np.float64
 
 def decorate(doc_top, top_words):
     # TODO make it so that smaller values are primarily the ones getting punished rather than the big values.
+    # either do modification followed by normalization
+    # - normalization will fuck up initial values so that big values become smaller, unless we modify everything
+    # or modifications with equal opposite modification elsewhere.
+    # - how to ensure that there is 'enough' to take from / give?
+    # IDEA: find bad values and remove them to create a 'bank',
+    # then increase lower values starting with the best similarity-value combos,
+    # until the bank runs dry
+    # (it might be better if the bank is a priority with some sort of cutoff to indicate when a transfer is no longer worth it)
+    # definition of high similarity?
+
     # construct topic-similarity matrix
-    topic_sim = np.zeros(shape=(len(top_words), len(top_words)))
-    for t1 in tqdm(range(len(top_words))):
-        for t2 in range(len(top_words)):
-            if t1 != t2 and topic_sim[t2, t1] == 0:
-                topic_sim[t1, t2] = hellinger_sim(top_words[t1], top_words[t2])
+    topic_sim = similarity_matrix(top_words, hellinger_sim)
     # Add add values of other topics based on similarity
     new_doc_top = np.copy(doc_top)
     new_doc_top = new_doc_top * new_doc_top
@@ -34,12 +40,35 @@ def decorate(doc_top, top_words):
     return new_doc_top
 
 
+def similarity_matrix(things, sim_func):
+    sim_matrix = np.zeros(shape=(len(things), len(things)))
+    for t1 in range(len(things)):
+        for t2 in range(len(things)):
+            if t1 != t2: #and sim_matrix[t2, t1] == 0:
+                sim_matrix[t1, t2] = sim_func(things[t1], things[t2])
+    return sim_matrix
+
+
+def decorate2(doc_top, top_words):
+    topic_sim = similarity_matrix(top_words, hellinger_sim)
+
+    new_doc_top = np.copy(doc_top)
+    # make priority list
+    # based on best match of topic value multiplied by similarity to that topic
+    # highest priority indexes should have their doc-top value increased, while low priority index should be lowered
+    priority = {i: max([topic_sim[i, i2] * doc_top[i2] for i2 in range(len(top_words)) if i != i2]) for i, x in enumerate(top_words)}
+    # TODO similarities are currently too similar to make any values stand out
+    print('test')
+    # TODO define cutoff
+    # TODO make trades
+
+
 def hellinger_dis(p, q):
     return norm(np.sqrt(p) - np.sqrt(q)) / _SQRT2
 
 
 def hellinger_sim(p, q):
-    return 1-hellinger_dis(p, q)
+    return 1 - hellinger_dis(p, q)
 
 
 if __name__ == '__main__':
@@ -55,15 +84,15 @@ if __name__ == '__main__':
     in_folder = "test"
     path = f"../model/generated_files/{in_folder}/"
 
-    with open(path+"wta.pickle", "rb") as file:
+    with open(path + "wta.pickle", "rb") as file:
         wta = pickle.load(file)
-    with open(path+"middle.pickle", "rb") as file:
+    with open(path + "middle.pickle", "rb") as file:
         middle = pickle.load(file)
-    with open(path+"topic_word.pickle", "rb") as file:
+    with open(path + "topic_word.pickle", "rb") as file:
         topic_word = pickle.load(file)
-    with open(path+"topic_word_dists.pickle", "rb") as file:
+    with open(path + "topic_word_dists.pickle", "rb") as file:
         top_word_dists = pickle.load(file)
-    with open(path+"document_topic_dists.pickle", "rb") as file:
+    with open(path + "document_topic_dists.pickle", "rb") as file:
         doc_top_dists = pickle.load(file)
 
     """
@@ -76,7 +105,7 @@ if __name__ == '__main__':
     
     decorate(doc_top, top_word)
     """
-    """
+
     # Dirichlet test case
     topic_num = 90
     word_num = 69192
@@ -84,10 +113,10 @@ if __name__ == '__main__':
     word_dir = dirichlet(np.full(word_num, 0.01))
     top_sample = top_dir.rvs(size=1)[0]
     word_samples = word_dir.rvs(size=topic_num)
-    decorate(top_sample, word_samples)
-    """
+    decorate2(top_sample, word_samples)
 
-    decorate(doc_top_dists[2][0], top_word_dists[2])
+
+    #decorate2(doc_top_dists[2][0], top_word_dists[2])
 
     # For others Measure distance between topic-distributions for the test document and other documents written by
     # the same author. NOTE even if two topics are using the same words, documents using these two topics separately
@@ -131,8 +160,6 @@ if __name__ == '__main__':
 
     # alternative add-on: decorate all doc-top distributions to increase values on topics that are similar to other topics with high value.
     print('hi')
-
-
 
     # For AT
     # Find the probability of each author having written each document
