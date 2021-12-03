34531ecfa6179f132f7813869139ba8b680d8be3
Peter Langballe Erichsen <p.erichsen@yahoo.dk>
2021-12-03 09:33:05 +0100

    update for lda output

--- model/gibbs_lda.py
+++ model/gibbs_lda.py
@@ -9,7 +9,7 @@ from gibbs_utility import increase_count, decrease_count, perplexity, get_cohere
     _conditional_distribution, compute_metrics_on_saved_model
 from model.save import Model
 from preprocess.preprocessing import prepro_file_load
-
+import pickle
 
 def random_initialize(documents: List[np.ndarray]):
     """
@@ -72,11 +72,11 @@ def gibbs_sampling(documents: List[np.ndarray],
 if __name__ == '__main__':
     alpha = 0.01
     beta = 0.1
-    iterationNum = 50
+    iterationNum = 100
     num_topics = 90
-    doc2word = list(prepro_file_load("doc2word").items())
-    doc2bow, dictionary, texts = prepro_file_load('doc2bow'), prepro_file_load('corpora'), list(
-        prepro_file_load('doc2pre_text').values())
+    doc2word = list(prepro_file_load("doc2word", folder_name='nice').items())
+    doc2bow, dictionary, texts = prepro_file_load('doc2bow', folder_name='nice'), prepro_file_load('corpora', folder_name='nice'), list(
+        prepro_file_load('doc2pre_text', folder_name='nice').values())
     D, W = (dictionary.num_docs, len(dictionary))
 
     train_docs, test_docs = train_test_split(doc2word, test_size=0.33, random_state=1337)
@@ -95,3 +95,34 @@ if __name__ == '__main__':
     # doc2word = list(prepro_file_load("doc2word").items())
     # train_docs, test_docs = train_test_split(doc2word, test_size=0.33, random_state=1337)
     # print(compute_metrics_on_saved_model("90_0.01_0.1_author_category_MultiModel", test_docs, True))
+
+    # calculate document-topic distribution
+    doc_top_dists = {}
+    for id, doc_info in tqdm(enumerate(word_topic_assignment)):
+        doc_dist = np.zeros(shape=num_topics)
+        for word in doc_info:
+            doc_dist[word] += 1
+        doc_top_dists[id] = doc_dist / doc_dist.sum()
+
+    # calculate topic-word distribution
+    top_word_dists = {i: np.zeros(W) for i in range(num_topics)}
+    for id, doc_info in tqdm(enumerate(word_topic_assignment)):
+        for word_pos, word_topic in enumerate(doc_info):
+            word_id = doc2word[id][1][word_pos]
+            top_word_dists[word_topic][word_id] += 1
+    for t in range(num_topics):
+        top_word_dists[t] = top_word_dists[t] / top_word_dists[t].sum()
+
+    out_folder = 'lda'
+    path = f"generated_files/{out_folder}/"
+
+    with open(path + "wta.pickle", "wb") as file:
+        pickle.dump(word_topic_assignment, file)
+    with open(path + "topic_word.pickle", "wb") as file:
+        pickle.dump(topic_word, file)
+    with open(path + "topic_to_word.pickle", "wb") as file:
+        pickle.dump(topic_word_c, file)
+    with open(path + "topic_word_dists.pickle", "wb") as file:
+        pickle.dump(top_word_dists, file)
+    with open(path + "document_topic_dists.pickle", "wb") as file:
+        pickle.dump(doc_top_dists, file)
